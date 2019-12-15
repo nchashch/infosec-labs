5cd3d9fd47a57c807d4c85aff343b19ebf0cc2f5
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-12-15 18:19:58 +0300

    Add initial implementation of caesar cipher

--- ceasar-cipher/app/Main.hs
+++ ceasar-cipher/app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
-import Lib
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Read as T
+import Lib (caesar)
 
 main :: IO ()
-main = someFunc
+main = do
+  fileName <- T.getLine
+  text <- T.readFile (T.unpack fileName)
+  let cipherText = caesar 10 text
+  T.putStrLn cipherText
+  T.putStrLn $ caesar (-10) cipherText
--- ceasar-cipher/src/Lib.hs
+++ ceasar-cipher/src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Text as T
+import Data.Char (chr, ord)
+
+normalize :: T.Text -> T.Text
+normalize = T.toLower . (T.filter isLatinLetter)
+
+isLatinLetter :: Char -> Bool
+isLatinLetter c = (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
+
+-- key is supposed to be a number between 0 and 25, if it is not key `mod` 26
+-- will be used
+caesar :: Int -> T.Text -> T.Text
+caesar key = (T.map encrypt) . normalize
+  where
+    -- NOTE: This function assumes that the latin alphabet is continuous and is
+    -- in order in the encoding used, which holds for UTF-8
+    encrypt :: Char -> Char
+    encrypt char = chr $ (ord char - ord 'a' + shift) `mod` 26 + ord 'a'
+      where
+        shift = key `mod` 26
