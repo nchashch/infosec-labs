1c27ae732defa5a8855de9e6bf9b7ff16b3aec9c
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2020-01-17 13:39:02 +0300

    Add frequency analysis in Freq.hs

--- ceasar-cipher/app/Main.hs
+++ ceasar-cipher/app/Main.hs
@@ -4,12 +4,19 @@ import System.Environment (getArgs)
 import qualified Data.Text as T (Text)
 import qualified Data.Text.IO as T (putStrLn, readFile)
 import Caesar (caesar)
+import Freq
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [fileName, key] -> encryptFile fileName (read key :: Int) >>= T.putStrLn
+    [fileName, key] -> do
+      cipherText <- encryptFile fileName (read key :: Int)
+      T.putStrLn cipherText
+      putStrLn ""
+      print freqOrder
+      print $ freqRank cipherText
+      print $ breakCaesarMode cipherText
     otherwise -> putStrLn "Invalid arguments\nUsage:\ncaesar fileName key"
 
 encryptFile :: FilePath -> Int -> IO T.Text
---
+++ ceasar-cipher/src/Freq.hs
@@ -0,0 +1,32 @@
+module Freq where
+
+import qualified Data.Text as T
+import qualified Data.HashMap.Strict as HMS
+import Data.Char (chr, ord)
+import Data.Sort
+import Data.List (group, sort)
+
+-- Ciphertext -> Key
+freqCount :: T.Text -> FreqDict
+freqCount text = T.foldr freqInc (HMS.empty :: FreqDict) text
+
+freqInc :: Char -> FreqDict -> FreqDict
+freqInc ch freqDict = HMS.insertWith (\new old -> new + old) ch 1 freqDict
+
+type FreqDict = HMS.HashMap Char Int
+
+freqOrder = "etaoinshrdlcumwfgypbvkjxqz"
+
+freqRank :: T.Text -> [Char]
+freqRank = reverse.(map fst).(sortOn snd).(HMS.toList).freqCount
+
+breakCaesar :: T.Text -> [Int]
+breakCaesar cipherText = zipWith (\a b -> ord a - ord b) freqOrder (freqRank cipherText)
+
+breakCaesarMode :: T.Text -> Int
+breakCaesarMode = head.mode.(map abs).breakCaesar
+
+mode :: (Ord a) => [a] -> [a]
+mode xs = map fst $ filter ((==best).snd) counts
+    where counts = map (\l -> (head l, length l)) . group . sort $ xs
+          best = maximum (map snd counts)
